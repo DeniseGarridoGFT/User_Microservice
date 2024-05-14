59df9255ddbf57881705d1b7d399b90cee803966
Jose.Luis.Tarraga <jsug@gft.com>
2024-05-14 15:17:04 +0200

    Creating tests for post WishList

---
+++ src/main/java/com/workshop/users/repositories/ProductRepositoryImpl.java
@@ -0,0 +1,27 @@
+package com.workshop.users.repositories;
+
+import com.workshop.users.api.dto.Product;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.server.ResponseStatusException;
+import reactor.core.publisher.Mono;
+
+@Repository
+public class ProductRepositoryImpl implements ProductRepository{
+    private final WebClient webClient;
+
+    public ProductRepositoryImpl() {
+        this.webClient = WebClient.builder().baseUrl("http://url_producto/api").build();
+    }
+
+    @Override
+    public Mono<Product> findProductById(Long id) throws ResponseStatusException {
+        return webClient.get()
+                .uri("/products/{id}", id)
+                .retrieve()
+                .bodyToMono(Product.class).onErrorMap(throwable -> {
+                    throw new ResponseStatusException(HttpStatus.NOT_FOUND,"The product not exists");
+                });
+    }
+}
