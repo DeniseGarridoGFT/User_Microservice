e547fafb2d6b7bd7ad91919a67b042342e2a69bc
Denise Garrido <degm@gft.com>
2024-05-23 12:55:26 +0200

    Pipeline Test Improvements (Not Date)

--- src/main/java/com/workshop/users/repositories/AddressDAORepository.java
+++ src/main/java/com/workshop/users/repositories/AddressDAORepository.java
@@ -1,11 +1,8 @@
 package com.workshop.users.repositories;
 
-import com.workshop.users.api.dto.AddressDto;
 import com.workshop.users.model.AddressEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.text.ParseException;
 
 @Repository
 public interface AddressDAORepository extends JpaRepository<AddressEntity,Long> {
