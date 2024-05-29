098d0b69430b3091a169abf43326fa72f8ce50c1
Denise Garrido <degm@gft.com>
2024-05-29 09:19:06 +0200

    Test schema with data 4

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -10,7 +10,7 @@ INSERT INTO countries (name, tax, prefix, time_zone) VALUES
 
 INSERT INTO addresses (city_name, number, door, street, zip_code) VALUES
     ('Madrid',32,'1A','C/ La Coma','47562');
-INSERT INTO ADDRESSES (city_name, number, door, street, zip_code) VALUES
+INSERT INTO addresses (city_name, number, door, street, zip_code) VALUES
         ('Madrid',32,'1A','C/ La Coma','47562');
 
 INSERT INTO users (name, last_name, email, password, fidelity_points, birth_date, phone,country_id,address_id)
