504f2f9866ff99df40093834fef1ec26ccf201f9
Denise Garrido <degm@gft.com>
2024-05-29 08:36:40 +0200

    Test schema with data 2

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -45,7 +45,7 @@ ALTER TABLE WISHES
 ADD CONSTRAINT FK_WISHES_USER
 FOREIGN KEY (USER_ID) REFERENCES USERS(ID);
 
-INSERT INTO Countries (name, tax, prefix, time_zone) VALUES
+INSERT INTO COUNTRIES (name, tax, prefix, time_zone) VALUES
     ('España', 21, '+34', 'Europe/Madrid'),
     ('Estonia', 20, '+372', 'Europe/Tallinn'),
     ('Finlandia', 24, '+358', 'Europe/Helsinki'),
@@ -60,7 +60,7 @@ INSERT INTO ADDRESSES (city_name, number, door, street, zip_code) VALUES
 INSERT INTO ADDRESSES (city_name, number, door, street, zip_code) VALUES
         ('Madrid',32,'1A','C/ La Coma','47562');
 
-INSERT INTO users (name, last_name, email, password, fidelity_points, birth_date, phone,country_id,address_id)
+INSERT INTO USERS (name, last_name, email, password, fidelity_points, birth_date, phone,country_id,address_id)
 VALUES ('Juan', 'García', 'juangarcia@example.com', '$2a$10$OyJUHBSm0sU8eF8os0ZuoOwDRmgg8ns4owWtIXItlYmN.1pDVxve6', 100, '1990-01-01', '123456789',1,1);
 INSERT INTO users (name, last_name, email, password, fidelity_points, birth_date, phone,country_id,address_id)
 VALUES ('Manolo', 'García', 'manolo@example.com', '$2a$10$OyJUHBSm0sU8eF8os0ZuoOwDRmgg8ns4owWtIXItlYmN.1pDVxve6', 100, '1990-01-01', '123456789',1,2);
